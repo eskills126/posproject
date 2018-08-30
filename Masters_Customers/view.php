cc8bbf3573688f2527f443d47b78776257ab7b17
eskills126 <eskills126@gmail.com>
2018-08-30 21:11:03 +0500

    unknown

--- Masters_Customers/view.php
+++ Masters_Customers/view.php
@@ -4,7 +4,6 @@
 	<title></title>
 
 
-
 <script src="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
