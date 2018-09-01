321aff100febb34298b851ffd61876345930c400
eskills126 <eskills126@gmail.com>
2018-09-01 12:47:40 +0500

    Databasae file also changed

--- Database/aansoftdb.sql
+++ Database/aansoftdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 30, 2018 at 02:40 PM
+-- Generation Time: Sep 01, 2018 at 09:46 AM
 -- Server version: 10.1.33-MariaDB
 -- PHP Version: 7.2.6
 
@@ -71,9 +71,9 @@ CREATE TABLE `customertbl` (
   `CusName` varchar(250) NOT NULL,
   `CusAddress` varchar(250) NOT NULL,
   `CusContact` varchar(250) NOT NULL,
-  `CusCreditLimit` int(50) NOT NULL,
+  `CusCreditLimit` int(50) DEFAULT NULL,
   `CusOpenBal` int(50) NOT NULL,
-  `CusAreaName` int(50) NOT NULL,
+  `CusAreaName` varchar(250) NOT NULL,
   `CusAccType` int(50) NOT NULL DEFAULT '1',
   `CusAssetType` int(50) NOT NULL DEFAULT '2'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -83,102 +83,101 @@ CREATE TABLE `customertbl` (
 --
 
 INSERT INTO `customertbl` (`CusId`, `CusName`, `CusAddress`, `CusContact`, `CusCreditLimit`, `CusOpenBal`, `CusAreaName`, `CusAccType`, `CusAssetType`) VALUES
-(1, 'Cash Sales', 'Vehari', '03136927444', 900, 10000, 1, 1, 2),
-(2, 'Take Away', 'Multan', '03115575561', 800, 15099, 1, 1, 2),
-(27, 'Naeem', 'ahmed', '897', 899, 89, 2, 1, 2),
-(28, 'Amjad', 'Hanif', '980', 899, 899, 2, 1, 2),
-(29, 'Arif', 'Hussain', '89', 89, 89, 2, 1, 2),
-(43, 'Rehman', 'Ali', '87', 87, 8, 2, 1, 2),
-(44, 'Name', 'no', '8', 7, 8, 1, 1, 2),
-(48, 'Naeem', 'u', '0673332312', 7, 8, 1, 1, 2),
-(51, 'lskdfj', 'alsdfjk', '8', 7, 6, 2, 1, 2),
-(52, 'james', 'bond', '8', 7, 6, 2, 1, 2),
-(53, 'Khan', 'imran', '8', 7, 6, 1, 1, 2),
-(54, 'Rate', 'ksdhh', '8', 7, 6, 2, 1, 2),
-(55, 'Lota ', 'virus', '8', 7, 6, 1, 1, 2),
-(56, 'Huawei', 'Mate', '7', 6, 5, 2, 1, 2),
-(57, 'Amjad', 'Hanif', '7', 6, 5, 2, 1, 2),
-(58, 'Karachi', 'khan', '8', 8, 7, 1, 1, 2),
-(59, 'Yes', 'No', '8', 7, 6, 1, 1, 2),
-(60, 'Javed ', 'sheikh', '8', 7, 6, 1, 1, 2),
-(68, 'Jamal', 'khan', '7', 6, 5, 2, 1, 2),
-(69, 'jaha', 'uy', '7', 6, 5, 1, 1, 2),
-(70, 'Set', 'mehtos', '8', 7, 6, 1, 1, 2),
-(71, 'skdfj', 'ksjdf', '8', 7, 6, 1, 1, 2),
-(72, 'kjsdf', 'klsjdf', '9', 8, 6, 1, 1, 2),
-(73, 'jah', 'ksd', '8', 7, 6, 1, 1, 2),
-(74, 'lksfjd', 'uyu', '7', 6, 6, 1, 1, 2),
-(75, 'sdf', 'ui', '78', 76, 7, 2, 1, 2),
-(76, 'klasj', 'u', '7', 7, 7, 1, 1, 2),
-(77, 'sdf', 'sfd', '9', 8, 7, 1, 1, 2),
-(78, 'sdf', 'wer', '2', 2, 2, 2, 1, 2),
-(79, 'jklsdfj', 'lksjdlk', '9', 8, 7, 1, 1, 2),
-(80, 'jakal', 'lksjd', '8', 7, 6, 1, 1, 2),
-(81, 'lskdfj', 'lksajdflk', '9', 8, 7, 1, 1, 2),
-(82, 'sfd', 'sfq9', '9', 8, 7, 1, 1, 2),
-(83, 'sdf', 'sfd', '9', 8, 7, 1, 1, 2),
-(84, 'sdad', 'jk', '8', 7, 6, 1, 1, 2),
-(85, 'sldkj', 'skdj', '8', 7, 76, 2, 1, 2),
-(86, 'safd', 'sfjk', '8', 7, 6, 1, 1, 2),
-(87, 'asf', 'asdf', '8', 7, 6, 1, 1, 2),
-(88, 'karachi', 'slakfjj', '8', 7, 6, 1, 1, 2),
-(89, 'Jamal', 'lskfjl', '9', 8, 7, 1, 1, 2),
-(90, 'amjad', 'u', '8', 7, 6, 1, 1, 2),
-(91, 'Naeem', '7', '6', 5, 8, 1, 1, 2),
-(92, 'Moin', 'u', '8', 7, 6, 1, 1, 2),
-(93, 'Mustajab', 'slfj', '8', 7, 6, 1, 1, 2),
-(94, 'Kashif', 'i', '8', 7, 6, 1, 1, 2),
-(95, 'u', 'k', '9', 8, 7, 1, 1, 2),
-(96, 'jug', 'i', '9', 8, 7, 1, 1, 2),
-(97, 'abcd', 'i', '8', 7, 6, 2, 1, 2),
-(98, 'khail', 'k', '9', 8, 7, 1, 1, 2),
-(99, 'customer', 'i', '8', 7, 6, 1, 1, 2),
-(100, 'klain', 'u', '8', 7, 6, 2, 1, 2),
-(101, 'yes', 'i', '8', 7, 6, 1, 1, 2),
-(102, 'not a ', 'u', '7', 6, 5, 1, 1, 2),
-(103, 'column', 'u', '8', 7, 6, 1, 1, 2),
-(104, 'All', 'u', '8', 7, 6, 2, 1, 2),
-(105, 'sdfklj', 'u', '9', 8, 6, 2, 1, 2),
-(106, 'james', 'skdfj', '8', 7, 6, 2, 1, 2),
-(107, 'kala', 'slfkjj', '9', 8, 67, 1, 1, 2),
-(108, 'sfd', 'l', 'i', 0, 8, 2, 1, 2),
-(109, 'jam', 'kja', '8', 7, 6, 2, 1, 2),
-(110, 's;dfkl', 'ui', '8', 7, 6, 2, 1, 2),
-(111, 'sljf', 'u', '7', 6, 5, 2, 1, 2),
-(112, 'sdfklj', 'i', 'u', 9, 8, 1, 1, 2),
-(113, 'asdf', 'jkj', '8', 7, 6, 1, 1, 2),
-(114, 'asfd', 'klsdjf', '9', 8, 7, 2, 1, 2),
-(115, 'asdf', '8', '7', 6, 5, 2, 1, 2),
-(116, 'sldfj', 'sldkfj', '9', 8, 7, 2, 1, 2),
-(117, 'sdfkj', 'sdlkfjh', '9', 8, 7, 1, 1, 2),
-(118, 'kaleem', 'u', '8', 7, 6, 1, 1, 2),
-(119, 'you', 'lskdfj', '9', 9, 8, 2, 1, 2),
-(120, 'Kamkaj', 'u', '8', 7, 6, 2, 1, 2),
-(121, 'kal', 'i', '8', 7, 6, 2, 1, 2),
-(122, 'kool', 'j', '8', 9, 8, 2, 1, 2),
-(123, 'slkfj', '9', '8', 7, 6, 2, 1, 2),
-(124, 'yes', 'i', '8', 7, 6, 2, 1, 2),
-(125, 'yahoo', 'i', '8', 7, 6, 1, 1, 2),
-(126, 'Yahoo2', 'asdlkj', '9', 8, 7, 1, 1, 2),
-(127, 'lkasjfd', 'sldkj', '8', 67, 6, 2, 1, 2),
-(128, 'yahoo3', 'askldj', '9', 8, 7, 2, 1, 2),
-(129, 'kahal', 'i', '8', 7, 6, 2, 1, 2),
-(130, 'kamal', 'u', '8', 7, 6, 2, 1, 2),
-(131, 'naeem', 'i', '9', 8, 7, 2, 1, 2),
-(132, 'Hos', 'ui', '8', 7, 6, 2, 1, 2),
-(133, 'Reh', 'slkj', '8', 9, 8, 2, 1, 2),
-(134, 'adf', 'lskj', '9', 8, 7, 2, 1, 2),
-(135, 'raja', 'iu', '8', 9, 7, 2, 1, 2),
-(136, 'khawaja', '89', '8', 7, 6, 2, 1, 2),
-(137, 'Rehman', 'skfj', '8', 7, 6, 1, 1, 2),
-(138, 'Javed', 'sldfkj', '8', 9, 78, 2, 1, 2),
-(139, 'Amjad', 'sldfj', '9', 8, 7, 2, 1, 2),
-(140, 'kashif', 'i', '9', 8, 7, 2, 1, 2),
-(141, 'Kala', 'slkj', '9', 8, 7, 2, 1, 2),
-(142, 'alsdfj', 'sdlkfj', '8', 9, 8, 2, 1, 2),
-(143, 'Yes', 'sdlkfj', '9', 8, 7, 2, 1, 2),
-(144, 'kjad', 'slakdj', '0', 9, 8, 1, 1, 2),
-(145, 'klhads', 'ksdjff', '9', 8, 7, 1, 1, 2);
+(1, 'Cash Sales', 'Vehari', '03136927444', 900, 10000, '1', 1, 2),
+(2, 'Take Away', 'Multan', '03115575561', 800, 15099, '1', 1, 2),
+(27, 'Naeem', 'ahmed', '897', 899, 89, '2', 1, 2),
+(28, 'Amjad', 'Hanif', '980', 899, 899, '2', 1, 2),
+(29, 'Arif', 'Hussain', '89', 89, 89, '2', 1, 2),
+(43, 'Rehman', 'Ali', '87', 87, 8, '2', 1, 2),
+(44, 'Name', 'no', '8', 7, 8, '1', 1, 2),
+(48, 'Naeem', 'u', '0673332312', 7, 8, '1', 1, 2),
+(51, 'lskdfj', 'alsdfjk', '8', 7, 6, '2', 1, 2),
+(52, 'james', 'bond', '8', 7, 6, '2', 1, 2),
+(53, 'Khan', 'imran', '8', 7, 6, '1', 1, 2),
+(54, 'Rate', 'ksdhh', '8', 7, 6, '2', 1, 2),
+(55, 'Lota ', 'virus', '8', 7, 6, '1', 1, 2),
+(56, 'Huawei', 'Mate', '7', 6, 5, '2', 1, 2),
+(57, 'Amjad', 'Hanif', '7', 6, 5, '2', 1, 2),
+(58, 'Karachi', 'khan', '8', 8, 7, '1', 1, 2),
+(59, 'Yes', 'No', '8', 7, 6, '1', 1, 2),
+(60, 'Javed ', 'sheikh', '8', 7, 6, '1', 1, 2),
+(68, 'Jamal', 'khan', '7', 6, 5, '2', 1, 2),
+(69, 'jaha', 'uy', '7', 6, 5, '1', 1, 2),
+(70, 'Set', 'mehtos', '8', 7, 6, '1', 1, 2),
+(71, 'skdfj', 'ksjdf', '8', 7, 6, '1', 1, 2),
+(72, 'kjsdf', 'klsjdf', '9', 8, 6, '1', 1, 2),
+(73, 'jah', 'ksd', '8', 7, 6, '1', 1, 2),
+(74, 'lksfjd', 'uyu', '7', 6, 6, '1', 1, 2),
+(75, 'sdf', 'ui', '78', 76, 7, '2', 1, 2),
+(76, 'klasj', 'u', '7', 7, 7, '1', 1, 2),
+(77, 'sdf', 'sfd', '9', 8, 7, '1', 1, 2),
+(78, 'sdf', 'wer', '2', 2, 2, '2', 1, 2),
+(79, 'jklsdfj', 'lksjdlk', '9', 8, 7, '1', 1, 2),
+(80, 'jakal', 'lksjd', '8', 7, 6, '1', 1, 2),
+(81, 'lskdfj', 'lksajdflk', '9', 8, 7, '1', 1, 2),
+(82, 'sfd', 'sfq9', '9', 8, 7, '1', 1, 2),
+(83, 'sdf', 'sfd', '9', 8, 7, '1', 1, 2),
+(84, 'sdad', 'jk', '8', 7, 6, '1', 1, 2),
+(85, 'sldkj', 'skdj', '8', 7, 76, '2', 1, 2),
+(86, 'safd', 'sfjk', '8', 7, 6, '1', 1, 2),
+(87, 'asf', 'asdf', '8', 7, 6, '1', 1, 2),
+(88, 'karachi', 'slakfjj', '8', 7, 6, '1', 1, 2),
+(89, 'Jamal', 'lskfjl', '9', 8, 7, '1', 1, 2),
+(90, 'amjad', 'u', '8', 7, 6, '1', 1, 2),
+(91, 'Naeem', '7', '6', 5, 8, '1', 1, 2),
+(92, 'Moin', 'u', '8', 7, 6, '1', 1, 2),
+(93, 'Mustajab', 'slfj', '8', 7, 6, '1', 1, 2),
+(94, 'Kashif', 'i', '8', 7, 6, '1', 1, 2),
+(95, 'u', 'k', '9', 8, 7, '1', 1, 2),
+(96, 'jug', 'i', '9', 8, 7, '1', 1, 2),
+(97, 'abcd', 'i', '8', 7, 6, '2', 1, 2),
+(98, 'khail', 'k', '9', 8, 7, '1', 1, 2),
+(99, 'customer', 'i', '8', 7, 6, '1', 1, 2),
+(100, 'klain', 'u', '8', 7, 6, '2', 1, 2),
+(101, 'yes', 'i', '8', 7, 6, '1', 1, 2),
+(102, 'not a ', 'u', '7', 6, 5, '1', 1, 2),
+(103, 'column', 'u', '8', 7, 6, '1', 1, 2),
+(104, 'All', 'u', '8', 7, 6, '2', 1, 2),
+(105, 'sdfklj', 'u', '9', 8, 6, '2', 1, 2),
+(106, 'james', 'skdfj', '8', 7, 6, '2', 1, 2),
+(107, 'kala', 'slfkjj', '9', 8, 67, '1', 1, 2),
+(108, 'sfd', 'l', 'i', 0, 8, '2', 1, 2),
+(109, 'jam', 'kja', '8', 7, 6, '2', 1, 2),
+(110, 's;dfkl', 'ui', '8', 7, 6, '2', 1, 2),
+(111, 'sljf', 'u', '7', 6, 5, '2', 1, 2),
+(112, 'sdfklj', 'i', 'u', 9, 8, '1', 1, 2),
+(113, 'asdf', 'jkj', '8', 7, 6, '1', 1, 2),
+(114, 'asfd', 'klsdjf', '9', 8, 7, '2', 1, 2),
+(115, 'asdf', '8', '7', 6, 5, '2', 1, 2),
+(116, 'sldfj', 'sldkfj', '9', 8, 7, '2', 1, 2),
+(117, 'sdfkj', 'sdlkfjh', '9', 8, 7, '1', 1, 2),
+(118, 'kaleem', 'u', '8', 7, 6, '1', 1, 2),
+(119, 'you', 'lskdfj', '9', 9, 8, '2', 1, 2),
+(120, 'Kamkaj', 'u', '8', 7, 6, '2', 1, 2),
+(121, 'kal', 'i', '8', 7, 6, '2', 1, 2),
+(122, 'kool', 'j', '8', 9, 8, '2', 1, 2),
+(123, 'slkfj', '9', '8', 7, 6, '2', 1, 2),
+(127, 'lkasjfd', 'sldkj', '8', 67, 6, '2', 1, 2),
+(128, 'yahoo and Gmail', 'askldj', '9', 8, 7, '2', 1, 2),
+(129, 'Mobile', 'vehari', '7', 8, 6, '2', 1, 2),
+(130, 'Mobile Phone', 'Lalazar', '87', 6, 8, '220 KV Grid Station', 1, 2),
+(131, 'Naeem', 'Ahmed', '', 9, 8, 'Vehari123', 1, 2),
+(132, 'Ready', '', '', 8, 7, 'Multan123', 1, 2),
+(133, 'Naeem', '', '', 0, 7, 'Vehari123', 1, 2),
+(134, 'Moin Again', '', '', 9, 8, 'Vehari123', 1, 2),
+(135, 'Imran Khan', '', '', 0, 900, 'Vehari', 1, 2),
+(136, 'Imran Khan', '', '', 8, 900, 'Vehari', 1, 2),
+(137, 'Ahmed', '', '', 0, 1200, 'VHR', 1, 2),
+(138, 'Jmal', '', '', 90, 3, '3', 1, 2),
+(139, 'Rehman', '', '', 0, 89, '7', 1, 2),
+(140, 'Kaleem', 'jan', '9800', 12500, 8, 'Vehari', 1, 2),
+(141, '89', '', '', 0, 9, 'o', 1, 2),
+(142, 'jkl', '', '', 0, 8, 'Vehari123', 1, 2),
+(143, 'abdul', '', '', 0, 9, '220 KV Grid Station', 1, 2),
+(144, 'NO Name', 'No Data', '', 0, 980, 'vhr', 1, 2),
+(145, 'James', 'Dan', '', 50, 8, '7', 1, 2),
+(146, 'Rao School', 'Rate', '879', 900, 8, 'Vehari123', 1, 2),
+(149, 'Amjad hanif', 'Red', '', 9, 8, 'Islamabad', 1, 2);
 
 -- --------------------------------------------------------
 
@@ -306,7 +305,11 @@ CREATE TABLE `salareatbl` (
 
 INSERT INTO `salareatbl` (`SalAreaId`, `SalAreaTitle`) VALUES
 (1, 'Vehari123'),
-(2, 'Multan123');
+(2, 'Multan123'),
+(3, '220 KV Grid Station'),
+(4, 'Islamabad'),
+(5, 'Karachi'),
+(6, 'Peshawar');
 
 -- --------------------------------------------------------
 
@@ -505,7 +508,7 @@ ALTER TABLE `assettbl`
 -- AUTO_INCREMENT for table `customertbl`
 --
 ALTER TABLE `customertbl`
-  MODIFY `CusId` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=146;
+  MODIFY `CusId` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=150;
 
 --
 -- AUTO_INCREMENT for table `exptbl`
@@ -547,7 +550,7 @@ ALTER TABLE `purchaseordertbl`
 -- AUTO_INCREMENT for table `salareatbl`
 --
 ALTER TABLE `salareatbl`
-  MODIFY `SalAreaId` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `SalAreaId` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `suptbl`
@@ -587,8 +590,7 @@ ALTER TABLE `assettbl`
 -- Constraints for table `customertbl`
 --
 ALTER TABLE `customertbl`
-  ADD CONSTRAINT `customertbl_ibfk_1` FOREIGN KEY (`CusAccType`) REFERENCES `acctypetbl` (`AccId`),
-  ADD CONSTRAINT `customertbl_ibfk_2` FOREIGN KEY (`CusAreaName`) REFERENCES `salareatbl` (`SalAreaId`);
+  ADD CONSTRAINT `customertbl_ibfk_1` FOREIGN KEY (`CusAccType`) REFERENCES `acctypetbl` (`AccId`);
 
 --
 -- Constraints for table `exptbl`
